afa5fee0d78aa07e196f227f70af5c3d58baeb76
leops1997 <leops1997@gmail.com>
2023-12-15 23:02:34 -0300

    first

---
+++ gridworld.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+
+"""
+Entrega #4 - Dynamic Programming - Policy Evaluation Gridworld
+
+@author: Leonardo Pezenatto da Silva
+@email: leonardo.pezenatto@posgrad.ufsc.br
+@date: Jun 20, 2023
+"""
+
+import numpy as np
+import matplotlib as plt
+
+def v_pl():
+    pass
+
+def policy_evaluation():
+    pass
+
+def plot_matrix():
+    pass
+
+def plot_t
+
+if __name__ == "__main__":
+ pass
